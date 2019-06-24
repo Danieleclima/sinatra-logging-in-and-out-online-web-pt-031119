1ef0f9775bdea506bacb283e794de08f1b5ffe47
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-06-24 18:30:11 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -4,7 +4,7 @@ def change
   create_table :users do |t|
     t.string :username
     t.string :password 
-    t.decimal :balance, precision: 10, scale: 1
+    t.decimal :balance, precision: 10, scale: 2
   end
 end
 end
